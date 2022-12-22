7e335320d16af2c11a455d13a356eddc3c8d3506
matthew-zeng <mathewzeng1@gmail.com>
2022-12-22 21:12:09 +0800

    backspace handle and equals handle

--- calculator.js
+++ calculator.js
@@ -1,5 +1,43 @@
+let buffer = '0'
+const screen = document.querySelector('.screen');
+
 function buttonClick(value) {
-  console.log(value);
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value)
+  }
+  rerender();
+}
+
+function handleNumber(number) {
+  if (buffer === '0') {
+    buffer = number;
+  } else {
+    buffer += number;
+  }
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case 'C':
+      buffer = '0';
+      break;
+    case '=':
+      console.log('equals');
+      break;
+    case '←':
+      if (buffer.length === 1) {
+        buffer = '0'
+      }
+      else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case '+':
+      console.log('plus');
+      break;
+  }
 }
 
 function init() {
@@ -8,6 +46,10 @@ function init() {
     .addEventListener("click", function (event) {
       buttonClick(event.target.innerText)
     })
+}
+
+function rerender() {
+  screen.innerText = buffer;
 }
 
 init();
